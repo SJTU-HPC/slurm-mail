3dc45b2b058f54005f1a0d2de53a379fa860da89
Nao-mii <993812697@qq.com>
2020-08-31 14:24:32 +0800

    Recover code about wallclockAccuracy & del crond sh

--- bin/clean_invalid_mail_spool.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-find /var/spool/slurm-mail/ -cmin +10 -exec rm  {} \;
--- bin/clean_slurm_mail_logs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo -n '' > /var/log/slurm-mail/slurm-send-mail.log
-echo -n '' > /var/log/slurm-mail/slurm-spool-mail.log
